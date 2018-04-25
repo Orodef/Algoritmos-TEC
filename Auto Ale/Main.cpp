76fb756160762e96ebe78f9f2718a494a234fea7
lucianoCR <36613181+lucianoCR@users.noreply.github.com>
2018-04-24 21:31:08 -0700

    Update Main.cpp

--- Auto Ale/Main.cpp
+++ Auto Ale/Main.cpp
@@ -621,11 +621,11 @@ void dibujarcarretera2() {
 }
 
 void animarCarretera(int vel) {
-	if (ncarretera1->y >= largoPantalla) {
+	if (ncarretera1->y >= largoPantalla-20) {
 		ncarretera1->y = -600;
 		ncarretera2->y = 0;
 	}
-	if (ncarretera2->y >= largoPantalla) {
+	if (ncarretera2->y >= largoPantalla-20) {
 		ncarretera2->y = -600;
 		ncarretera1->y = 0;
 	}
